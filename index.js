b36ac7f711c43abe343f2f0b677df6917f79f44e
Hibatica <michaelappleton.maa@gmail.com>
2023-04-25 21:19:36 -0500

    set token for test or produciton mode

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const mode = "test"; // "test" or "production"
+const commandHandler = require("./index/commandHandler.js");
+const eventHandler = require("./index/eventHandler.js");
+
+// set token to test bot token or production bot token
+const token;
+if(mode == "test") {
+	token = require("./config/auth/discordToken.json").token.test;
+} else if(mode == "production") {
+	token = require("./config/auth/discordToken.json").token.production;
+} else {
+	console.log("Please set 'mode' in index.js to either 'test' or 'production");
+	process.exit(1);
+}
